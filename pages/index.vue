bb63d7e17bac0ccff4b580848f21ea55b4b0d434
nerocodes <neropaulej@gmail.com>
2019-06-14 20:03:45 +0100

    feature: added right side

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,28 @@
 <template>
   <section class="homepage">
     <div class="left">
-      
+      <div class="img">
+        <img src="img/teeloni.png" alt="Teeloni">
+      </div>
     </div>
     <div class="right">
-
+      <div class="icons">
+        <div class="search">
+          <i class="fas fa-search"></i>
+        </div>
+        <div class="social">
+          <i class="fab fa-facebook-square"></i>
+          <i class="fab fa-instagram"></i>
+          <i class="fab fa-twitter"></i>
+        </div>
+      </div>
+      <div class="img">
+        <img src="img/rack.png" alt="Rack">
+      </div>
+      <div class="message">
+        <img src="img/msg.jpg" alt="message icon">
+        <p>Talk to us</p>
+      </div>
     </div>
   </section>
 </template>
