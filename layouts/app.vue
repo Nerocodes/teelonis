851004832618b8743f71e20c64cc5b8233817c3c
Oghenero Paul-Ejukorlem <ogheneropaul-ejukorlem@Ogheneros-MacBook-Pro.local>
2019-07-08 20:12:42 +0100

    feature: added experience us page

--- layouts/app.vue
+++ layouts/app.vue
@@ -5,7 +5,6 @@
     <main>
       <nuxt/>
     </main>
-
     <Footer/>
   </section>
 </template>
@@ -18,6 +17,17 @@ export default {
   components: {
     Nav,
     Footer
+  },
+  mounted() {
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5d21611822d70e36c2a47dd9/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
   }
 }
 </script>
